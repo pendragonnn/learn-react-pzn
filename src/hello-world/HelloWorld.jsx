e611428814b1d61b34ede08380dbeeebae89b490
Wisnu Andika <124800648+pendragonnn@users.noreply.github.com>
2025-09-15 09:03:55 +0700

    pzn-rd-002: Component

--- src/hello-world/HelloWorld.jsx
+++ src/hello-world/HelloWorld.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
+// COMPONENT
+/*
+  - Reaact Component mendukung semua elemen HTML dan SVG
+  - jadi kita tidak perlu khawatir ketika membuat Component, karena semua element pasti didukung
+*/
 
 export default function HelloWorld() {
   return (
     <div>
-      <h1>Hello World</h1>
-      <p>Happy Learn React</p>
+      <HeaderHelloWorld />
+      <ParagraphHelloWorld />
     </div>
+  )
+}
+
+// MULTIPLE COMPONENT
+/*
+  - Sebelumnya kita membuat satu file JSX untuk satu Component
+  - Walaupun itu adalah praktek yang baik, tapi bukan berarti itu wajib dilakukan
+  - Componenet sebenarnya hanyalah sebuah function yang mengembalikan React Element, oleh karena itu jika kita ingin membuat Component, kita hanya cukup membuat function yang mengembalikan React Element
+*/
+
+function HeaderHelloWorld() {
+  return (
+    <h1>Hello World</h1>
+  )
+}
+
+function ParagraphHelloWorld() {
+  return (
+    <p>Selamat Belajar ReactJS bro</p>
   )
 }
